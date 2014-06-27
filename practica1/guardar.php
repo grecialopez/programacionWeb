840816b7eb50da5d05066278c5b24fc58ceb9275
grecia <grecia10@hotmail.com>
2014-06-27 16:21:27 -0500

    primer commit

---
+++ practica1/guardar.php
@@ -0,0 +1,19 @@
+<?php
+echo "HOLA...";
+echo "goles Equipo 1 Local:" . $_POST['equipo1_local'];
+echo "<br />";
+echo "Goles Equipo 2 Visitante:" . $_POST['equipo2_visitante'];
+echo "<br />";
+echo "Goles Equipo 3 Local:" . $_POST['equipo3_local'];
+echo "<br />";
+
+echo "Equipo Elegido:" . $_POST['selectEquipo5_local'];
+echo "<br />";
+
+echo "Equipo Elegido:" . $_POST['selectEquipo6_visitante'];
+echo "<br />";
+echo "Equipo Elegido:" . $_POST['equipo4_visitante'];
+
+echo "<br />";
+?>
+
